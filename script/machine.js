2b1126c1e51b10cbae94e454c914399df3e5183a
Azadhossain288 <azadhossain016288@gmail.com>
2026-02-25 14:34:26 +0600

    added some payment

--- script/machine.js
+++ script/machine.js
@@ -36,14 +36,16 @@ function showOnly(id){
       const addmoney=document.getElementById("add-money");
       const cashout=document.getElementById("cashout");
       const sendMoney=document.getElementById("transfer-money");
+      const payMoney=document.getElementById("Pay-bill");
       const history=document.getElementById("history");
-      console.log(`add Money- ${addmoney},Cashout-${cashout} ,History-${history},Transfer-${sendMoney}`);
+      console.log(`add Money- ${addmoney},Cashout-${cashout} ,History-${history},Transfer-${sendMoney},Payment-${payMoney}`);
 
       // All hide
 
       addmoney.classList.add("hidden");
       cashout.classList.add("hidden");
       sendMoney.classList.add("hidden");
+      payMoney.classList.add("hidden");
       history.classList.add("hidden");
 
       // remove selected id of hide
---
+++ script/paybill.js
@@ -0,0 +1,24 @@
+document.getElementById("payMoney-btn").addEventListener("click",function(){
+
+
+   // 1- bank account get
+
+    const bankAccount=getValueFromInput("select-payment");
+    if(bankAccount== "Select a payment method"){
+          alert("please select a payment method");
+          return;
+    }
+
+    // 2- get bank account number
+
+    
+
+   
+
+    
+
+
+
+
+
+})
